78a2c343a9303ae7eaf5e27675743b1e90e9c502
AnaBatinovic <ana.batinovic.ab@gmail.com>
2023-02-21 12:07:57 +0100

    Change odom topic

--- startup/uav_indoor/orbslam3_setup.sh
+++ startup/uav_indoor/orbslam3_setup.sh
@@ -2,7 +2,7 @@ export UAV_NAMESPACE=red
 export PIX_SYM=/dev/ttyUSB_px4:921600
 
 #export SENSOR=orb_slam3
-export ODOM_TOPIC=/orb_slam3/odom
+export ODOM_TOPIC=/$UAV_NAMESPACE/es_ekf/odom
 export IMU_TOPIC=/$UAV_NAMESPACE/lpms/imu/data
 
 export CONTROL_TYPE=pid_cascade_node_yawrate
