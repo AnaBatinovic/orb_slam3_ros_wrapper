79bf68ffdaf479286316f7f08d81cef04f40526e
Ana Batinovic <ana.batinovic.ab@gmail.com>
2023-02-21 12:13:05 +0100

    Keep optitrack

--- startup/uav_indoor/orbslam3_setup.sh
+++ startup/uav_indoor/orbslam3_setup.sh
@@ -4,6 +4,8 @@ export PIX_SYM=/dev/ttyUSB_px4:921600
 #export SENSOR=orb_slam3
 export ODOM_TOPIC=/$UAV_NAMESPACE/es_ekf/odom
 export IMU_TOPIC=/$UAV_NAMESPACE/lpms/imu/data
+export OBJECT_NAME=duckpink
+export OPTITRACK_IP=192.168.1.50
 
 export CONTROL_TYPE=pid_cascade_node_yawrate
 export CONTROL_PARAMS=custom_config/optitrack_position_control_duck.params.yaml
