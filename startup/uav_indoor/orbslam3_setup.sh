8c5b2c78967079339e3844ca46e3a9ac1dc31b51
Ana Batinovic <ana.batinovic.ab@gmail.com>
2023-02-22 16:23:36 +0100

    Creat slam rgbd launch and config files, set slam position control params

--- startup/uav_indoor/orbslam3_setup.sh
+++ startup/uav_indoor/orbslam3_setup.sh
@@ -3,12 +3,12 @@ export PIX_SYM=/dev/ttyUSB_px4:921600
 
 #export SENSOR=orb_slam3
 export ODOM_TOPIC=/$UAV_NAMESPACE/es_ekf/odom
-export IMU_TOPIC=/$UAV_NAMESPACE/lpms/imu/data
+export IMU_TOPIC=/$UAV_NAMESPACE/mavros/imu/data
 export OBJECT_NAME=duckpink
 export OPTITRACK_IP=192.168.1.50
 
 export CONTROL_TYPE=pid_cascade_node_yawrate
-export CONTROL_PARAMS=custom_config/optitrack_position_control_duck.params.yaml
+export CONTROL_PARAMS=custom_config/slam_position_control_duck.params.yaml
 export RC_MAPPING=
 
 # Sensor fusion config
